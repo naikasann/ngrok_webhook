c634d2138f99a7f8a85e533c1fad37e4280360a6
Yamazaki Kohei <51977859+naikasann@users.noreply.github.com>
2020-09-19 12:19:12 +0900

    :art: rename file(my handler)

--- myhandler/MyHandler.py
+++ myhandler/MyHandler.py
@@ -1,8 +1,10 @@
 import http.server
-import socketserver
 import json
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
+    def __init__(self):
+        pass
+
     def do_POST(self):
         self.send_response(200)
         self.end_headers()
