d3500310da7040169fe3013392e5a09a2958fbb5
yamazaki <51977859+naikasann@users.noreply.github.com>
2020-09-24 00:55:52 +0900

    :boom: :ambulance: :construction: Flask app update

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-from myhandler.MyHandler import MyHandler
-import socketserver
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import jsonify
 
-def create_httpserver():
-    with socketserver.TCPServer(("", 80), MyHandler) as http:
-        http.serve_forever()
+app = Flask(__name__)
 
+@app.route("/")
+def top_page():
+    # TODO : html create.
+    return render_template('top_page.html', title='test server')
+
+@app.route("/user/<name>")
+def user_data():
+    # TODO : You can view details of the user's information.
+    # TODO : html create.
+    # NOW => error ;<
+    return render_template('user.html', title='test server')
+
+@app.route("/data_form", methods=["POST"])
+def data_form():
+    if request.method == 'POST':
+        # TODO : Register with databases and other data management systems.
+        result = "ok."
+    else:
+        result = "dont method request."
+    return jsonify({
+            'status':'OK',
+            'result': result
+        })
+
+## おまじない
 if __name__ == "__main__":
-    create_httpserver()
+    app.run(debug=True)
