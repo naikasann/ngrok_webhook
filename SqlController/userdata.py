1ad55655841afeb37a2a7512d5d7d54e3b419d29
b1745980 <72068864+b1745980@users.noreply.github.com>
2020-10-02 05:19:41 +0900

    データベースのユーザー情報の構造

--- SqlController/userdata.py
+++ SqlController/userdata.py
@@ -1,12 +1,26 @@
 #model class
+from sqlalchemy import Column,Integer,String,Text
+from SqlController.database import Base
+
+from SqlController.database import init_db
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine,Column,Integer,String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound
 class UserData(Base):
-    __talename__="userdata"
+    __tablename__="userdata"
 
     id=Column(String,primary_key=True)
     device_id=Column(String(255))
     password=Column(String(255))
+    init_db()
+    def __init__(self,id=None,device_id=None,password=None):
+        self.id=id
+        self.device_id=device_id
+        self.password=password
 
-    #get Dict data
+   #get Dict data
     def toDict(self):
         return{
             "id":str(self.id),
@@ -23,8 +37,9 @@ class UserData(Base):
     
     #get all userdata record
     def getAll():
+        engine=create_engine("sqlite:///user.sqlite3")
         Session=sessionmaker(bind=engine)
         ses=Session()
-        res=ses.query(Mydata).all()
+        res=ses.query(UserData).all()
         ses.close()
         return res
