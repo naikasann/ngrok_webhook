f339de50055ef855239c8ff5300a444d30a78154
Yamazaki Kohei <51977859+naikasann@users.noreply.github.com>
2020-10-02 15:18:10 +0900

    :zap::recycle: リファクタリングした

--- SqlController/MysqlController.py
+++ SqlController/MysqlController.py
@@ -1,5 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session,sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 class MysqlController:
     def __init__(self):
- 
-        pass
+        # Database initialize.
+        engine=create_engine("sqlite:///user.sqlite3")
+        db_session=scoped_session(sessionmaker(bind=engine))
+        Base=declarative_base()
+        Base.metadata.create_all(bind=engine)
--- SqlController/userdata.py
+++ SqlController/userdata.py
@@ -1,42 +1,61 @@
 #model class
 from sqlalchemy import Column,Integer,String,Text
-from SqlController.database import Base
-
-from SqlController.database import init_db
-
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine,Column,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.exc import NoResultFound
+
+from SqlController.MysqlController import MysqlController
+
 class UserData(Base):
+    # sql database table name.
     __tablename__="userdata"
 
+    # Declaration of each column in the userdata table 
     id=Column(String,primary_key=True)
     device_id=Column(String(255))
     password=Column(String(255))
-    init_db()
+    # database class cast.
+    sqlcontroller = MysqlController()
+
     def __init__(self,id=None,device_id=None,password=None):
+        """
+        function : Initialize the class.
+                　　It also declares the variables in the class.
+        """
         self.id=id
         self.device_id=device_id
         self.password=password
 
-   #get Dict data
     def toDict(self):
+        """
+        function :　Each value of the table is returned as a dictionary type.
+                    (WARNING : Since we return a variable in the class,
+                    we move the function to be retrieved before that.)
+        return   :  table variable.
+        """
         return{
             "id":str(self.id),
             "device_id":str(self.device_id),
             "password":str(self.password)
         }
-    
-    #get List data
+
     def getByList(arr):
+        """
+        function :　Converts the specified list to a dictionary list and returns it.
+        return   :  table variable.(list<dict>)
+        """
         res=[]
         for item in arr:
             res.append(item.toDict())
         return res
-    
+
     #get all userdata record
     def getAll():
+        """
+        function :　Retrieve all columns of the user table
+        return   :  All data in the usertable. (maybe : list?)
+        """
         engine=create_engine("sqlite:///user.sqlite3")
         Session=sessionmaker(bind=engine)
         ses=Session()
