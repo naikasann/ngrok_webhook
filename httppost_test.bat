a6c58dbbfd0ad190455996679b29e4932dfe60e1
yamazaki <51977859+naikasann@users.noreply.github.com>
2020-10-29 21:44:44 +0900

    :zap: httpPOSTの値をより実数的なものに

--- httppost_test.bat
+++ httppost_test.bat
@@ -1,5 +1,5 @@
 cls
 @echo off
 set /P URLREQUEST="URL‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢ : "
-curl -X POST %URLREQUEST% -d "device=2&time=2019-01-01 12:00:00&data=ababababbababab"
+curl -X POST %URLREQUEST% -d "device=7459E1&time=1603902940&data=12344"
 @pause
