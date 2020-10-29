afd3a4e3bd6f7f0c916c246f65604afa8c921c18
yamazaki <51977859+naikasann@users.noreply.github.com>
2020-10-29 21:46:23 +0900

    :construction: デバッグ環境に合わせて

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from flask import session
 from flask import send_from_directory
 
 import os
+import json
+import datetime
 
-from SqlController.receivetable import ReceiveData
-from SqlController.userdata import UserData
+from Database.users import Users
+from Database.postdatas import PostDatas
+from Database.gpsdatas import GpsDatas
 
 app = Flask(__name__)
 app.secret_key=b"888888"
@@ -37,12 +40,19 @@ def get_username(islogin):
 # Page before login
 ################################################################
 # top page.
+@app.route("/debug")
+def debug_page():
+    alldata=Users.getAll()
+    print("alldata", alldata)
+    return render_template("/debug.html", title="なにかのページ", all_userdata=alldata)
+
+# top page.
 @app.route("/")
 def top_page():
     # session login check.
     if isLogin():
-        alldata=ReceiveData.getAll()
-        return render_template("/userpage/data.html", title="なにかのページ", all_userdata=alldata, login_flg=False, login_user=get_username(True))
+        alldata=PostDatas.getAll()
+        return render_template("/userpage/data.html", title="なにかのページ", all_userdata=alldata, login_flg=True, login_user=get_username(True))
     else:
         return redirect("/login")
 
@@ -62,7 +72,7 @@ def login_post():
     # Get the id and password from the http post.
     request_id=request.form.get("id")
     request_password=request.form.get("pass")
-    if(UserData.dologin(request_id, request_password)):
+    if(Users.doLogin(request_id, request_password)):
         # Record login information in the session.
         session["login"]=True
         session["id"]=str(request_id)
@@ -89,7 +99,7 @@ def signup_post():
     request_device_id=request.form.get("device_id")
     request_password=request.form.get("pass")
     # do database resister.
-    isRegister, errormsg = UserData.makeRegistration(request_id, request_device_id, request_password)
+    isRegister, errormsg = Users.makeRegistration(request_id, request_device_id, request_password)
     if(isRegister):
         # Record login information in the session.
         session["login"]=True
@@ -105,18 +115,36 @@ def userdata():
 ################################################################
 # Http request page.
 ################################################################
-@app.route("/data_form", methods = ["POST"])
+@app.route("/dataform", methods = ["POST"])
 def data_form():
+    # communicating with the correct means of request?
     if request.method == 'POST':
-        # TODO : Register with databases and other data management systems.
-        result = "ok."
-        print(request.form)
-    else:
-        result = "dont method request."
-    return jsonify({
-            'status':'OK',
-            'result': result
-        })
+        # Find out who sent it (only SIGFOX will receive it this time)
+        if request.headers.get('User-Agent') == "SIGFOX":
+            # Retrieve the body information and convert it to json format
+            request_data = json.loads(request.data)
+            # Retrieving the data needed for the database. (Device ID, receive time, actual raw data)
+            device_id = request_data["device"]
+            recv_time = datetime.datetime.fromtimestamp(request_data["time"])
+            data = request_data["data"]
+            # Extracting location information from json. If have GPS => Location information acquisition()
+            if request_data["computedLocation"]["status"] == 1:
+                lat, lng = (int(request_data["computedLocation"]["lat"]), int(request_data["computedLocation"]["lng"]))
+                # Radius of error (meters)
+                radius = int(request_data["computedLocation"]["radius"])
+            else:
+                # donthave => Register Null Island(joke :DDDDDDDDDDDDDDD)
+                lat, lng = (0, 0)
+                radius = 0
+            # Add to the database => postdatas, gpsdatas, rawdatas
+            PostDatas.doRecord(device_id, data, recv_time)
+            dataid = PostDatas.getLastDataid()
+            GpsDatas.doRecord(lat, lng, radius, dataid[0])
+        else:
+            # If the POST communication was from a different agent.This time you reply with a json and do nothing.
+            return jsonify({'status':'It is sent by an unregistered agent. We are unable to receive it.'})
+
+    return jsonify({'status':'OK'})
 
 @app.route("/user_state", methods = ["POST"])
 def get_user_state():
@@ -124,7 +152,6 @@ def get_user_state():
     if request.method == "POST":
         requeststate = "ok"
         result = "not complete this page..."
-        pass
     else:
         requeststate = "ng"
         result = "You are trying to enter in a way that your administrator has not yet."
